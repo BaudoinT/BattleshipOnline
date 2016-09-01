cfe5dcb9df721ff1dca44cc6b7fcca4a250d1fdd
Cyril Gerard <gerardcyril19@gmail.com>
2016-09-01 08:45:38 +0200

    voici ma cellule

---
+++ batailleNaval/src/bataillenaval/Cellule.java
@@ -0,0 +1,39 @@
+package bataillenaval;
+
+public class Cellule {
+	
+	int x;
+	int y;
+	boolean toucher = false;
+	
+	public Cellule(int x, int y, boolean toucher) {
+		this.x = x;
+		this.y = y;
+		this.toucher = toucher;
+	}
+	
+	public int getX() {
+		return x;
+	}
+	
+	public void setX(int x) {
+		this.x = x;
+	}
+	
+	public int getY() {
+		return y;
+	}
+	
+	public void setY(int y) {
+		this.y = y;
+	}
+	
+	public boolean isToucher() {
+		return toucher;
+	}
+	
+	public void setToucher(boolean toucher) {
+		this.toucher = toucher;
+	}
+	
+}
