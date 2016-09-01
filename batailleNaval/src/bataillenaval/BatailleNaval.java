1bd7ec7642151c69a498848d5ea6b3d17d1f51b8
Dostoi <Dostoi@users.noreply.github.com>
2016-09-01 11:35:55 +0200

    Update BatailleNaval.java

--- batailleNaval/src/bataillenaval/BatailleNaval.java
+++ batailleNaval/src/bataillenaval/BatailleNaval.java
@@ -1,26 +1,26 @@
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 
-package bataillenaval;
+package reseau;
+
+import bataillenaval.Plateau;
+import com.google.gson.GsonBuilder;
 
 /**
  *
  * @author lalleaul
  */
-public class BatailleNaval {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        System.out.println(constantes.HELP);
-        System.out.println("Entrez JOUER pour debuter une nouvelle partie.");
-        System.out.println("Entrez REPRENDRE pour reprendre une partie.");
-        new ZoneSaisie();
-        
-    }
-    
+public class test {
+ public static void main(String[] args){
+     Writer w = new Writer();
+     Plateau p = new Plateau();
+     p.initPlateauAvecDesTrucs();
+     w.write("testonsGayment", new GsonBuilder().create().toJson(p));
+     w.delete("testonsGayment");
+ }
 }
+
