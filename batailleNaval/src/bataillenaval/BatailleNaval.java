798fcb338a8224d08f32924103981d1dc8d86941
William <william.jedrzejak@gmail.com>
2016-09-01 15:05:25 +0200

    update saisie

--- batailleNaval/src/bataillenaval/BatailleNaval.java
+++ batailleNaval/src/bataillenaval/BatailleNaval.java
@@ -23,9 +23,9 @@ public class BatailleNaval {
 
         System.out.println(constantes.HELP);
         System.out.println("Entrez JOUER pour debuter une nouvelle partie.");
-//        new ZoneSaisie();
+        new ZoneSaisie();
      
         aff.affichagePlateau();
-    }
+    } 
     
 }
