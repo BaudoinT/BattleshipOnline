af0e110b5bccdf5edbca1c6c50050bc43c510308
Baudoin <baudointt@gmail.com>
2016-09-01 10:07:17 +0200

    bfsfgb

--- batailleNaval/src/bataillenaval/Bateau.java
+++ batailleNaval/src/bataillenaval/Bateau.java
@@ -1,40 +1,22 @@
 package bataillenaval;
 
-import java.util.ArrayList;
-
 public class Bateau {
-
-	private boolean coule;
-	private Cellule[] emplacement;
-
-	public Bateau(int x, int y, String direction, int longueur){
-		coule=false;
-		emplacement= new Cellule[longueur];
-		for(int i=0; i<longueur;i++){
-			emplacement[i]=new Cellule(x, y, false);
-			if(direction.equals("vertical")){
-				y++;
-			}else{
-				x++;
-			}
-		}
+	
+	private int nbVie;
+	
+	public Bateau(int longueur){
+		nbVie=longueur;
 	}
 	
-	public boolean bateauCoule(){
-		for(int i=0; i<emplacement.length;i++){
-			if(!emplacement[i].isToucher()){
-				return false;
-			}
-		}
-		return true;
+	public void bateauCoule(){
+		//afficher message
 	}
 	
-	public void bateauTouche(int x, int y){
-		for(int i=0; i<emplacement.length;i++){
-			if(emplacement[i].getX()==x && emplacement[i].getY()==y){
-				emplacement[i].setToucher(true);
-			}
+	public void bateauTouche(){
+		nbVie--;
+		//afficher message
+		if(nbVie==0){
+			bateauCoule();
 		}
 	}
-	
 }
--- batailleNaval/src/bataillenaval/constantes.java
+++ batailleNaval/src/bataillenaval/constantes.java
@@ -1,8 +1,8 @@
 
-package bataillenaval;
+	package bataillenaval;
 
 public final class constantes {
-     
+    
     public static final int A = 1;
     public static final int B = 2;
     public static final int C = 3;
