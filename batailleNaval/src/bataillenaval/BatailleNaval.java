90fcaafb1c6f217aa7c3417f779fdbe32db6bf34
William <william.jedrzejak@gmail.com>
2016-09-02 09:53:08 +0200

    menu en cours

--- batailleNaval/src/bataillenaval/BatailleNaval.java
+++ batailleNaval/src/bataillenaval/BatailleNaval.java
@@ -6,6 +6,11 @@
 
 package bataillenaval;
 
+import com.google.gson.GsonBuilder;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import reseau.FileManager;
+
 /**
  *
  * @author lalleaul
@@ -26,12 +31,31 @@ public class BatailleNaval {
         System.out.println("1: Jouer en partie local");
         System.out.println("2: Jouer en partie online");
         System.out.println("3: Rejoindre une partie online");
+        FileManager fM = new FileManager();
         Saisir saisie = new Saisir();
-        if(saisie.choixListe(3) == 1){
-             Jeu jeu= new Jeu();
+        int choix = saisie.choixListe(3);
+        if (choix == 1) {
+            Jeu jeu = new Jeu();
              jeu.lancerPartie();
+        } else if (choix == 2) {
+            String gameName = fM.createNewGame();
+            while (!fM.isMyTurn()) {
+                try {
+                    Thread.sleep(5000);
+                } catch (InterruptedException ex) {
         }
+            }
+            Plateau plateau = new Plateau();
 
+            Partie partie = new Partie(plateau, gameName, System.getProperty("user.name"), "unknown");
        
+            fM.write(gameName, new GsonBuilder().create().toJson(partie));
+            
+            
+        } else if (choix == 3) {
+            fM.joinGame();
+
     }
+
+}
 }
