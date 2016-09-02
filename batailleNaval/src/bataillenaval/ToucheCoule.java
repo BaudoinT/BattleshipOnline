90c3d7bea3dce12e8382ae014fdde49adcb58d6e
Collot.Louis-Arthur <louis-arthur.collot@etudiant.univ-lille1.fr>
2016-09-02 11:00:59 +0200

    fluidité

--- batailleNaval/src/bataillenaval/ToucheCoule.java
+++ batailleNaval/src/bataillenaval/ToucheCoule.java
@@ -3,53 +3,63 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package bataillenaval;
 
 import java.util.ArrayList;
 
 public class ToucheCoule {
-    
-    private ArrayList<Cellule[][]> plateaux=new ArrayList<>();
+
+    private ArrayList<Cellule[][]> plateaux = new ArrayList<>();
     private Saisir saisie;
     private Affichage aff;
-    
-    public ToucheCoule(Plateau plateau,Affichage aff){
-       plateaux.add(plateau.getplateauJ1());
-       plateaux.add(plateau.getplateauJ2());
-       saisie=new Saisir();
-       this.aff=aff;
+
+    public ToucheCoule(Plateau plateau, Affichage aff) {
+        plateaux.add(plateau.getplateauJ1());
+        plateaux.add(plateau.getplateauJ2());
+        saisie = new Saisir();
+        this.aff = aff;
     }
-    
-    public void Attaque(int tour){
-       System.out.println("\nA l'autre joueur !\nChoisis une case a attaquer: ");              
-       saisie.choixCase();
-       if(plateaux.get(1-tour)[saisie.getLigne()][saisie.getCol()].setToucher()){
-           aff.affichagePlateau(tour);
-           System.out.println("Tu as touché!");
-           // ajout 
+
+    public void Attaque(int tour) {
+        aff.affichagePlateau(tour);
+        System.out.println("\nA l'autre joueur !\nChoisis une case a attaquer: ");
+        saisie.choixCase();
+        if (plateaux.get(1 - tour)[saisie.getLigne()][saisie.getCol()].setToucher()) {
+            aff.affichagePlateau(tour);
+            System.out.println("Tu as touché!");
+            // ajout 
         } else {
-           aff.affichagePlateau(tour);
-           System.out.println("A l'eau!");
-       }
-    }
-    
-    public boolean gagne(int tour){
-        int x=0,y=0;
-        boolean bateauVivant=false;
+            aff.affichagePlateau(tour);
+            System.out.println("A l'eau!");
+        }
+        aff.affichagePlateau(tour);
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException ex){
         
-        do{
-           if(plateaux.get(tour)[x][y].hasBateau() && !plateaux.get(tour)[x][y].bateauCoule())
-               bateauVivant = true;
-           if(x<10)
-               x++;
-           else{
-               x=0;
-               y++;
-           }   
-        }while((x<10 && y<10) && !bateauVivant);
+        } 
+        }
+    
+    
+
+    
+
+    public boolean gagne(int tour) {
+        int x = 0, y = 0;
+        boolean bateauVivant = false;
+
+        do {
+            if (plateaux.get(tour)[x][y].hasBateau() && !plateaux.get(tour)[x][y].bateauCoule()) {
+                bateauVivant = true;
+            }
+            if (x < 10) {
+                x++;
+            } else {
+                x = 0;
+                y++;
+            }
+        } while ((x < 10 && y < 10) && !bateauVivant);
         return bateauVivant;
     }
-    
-}
 
+}
