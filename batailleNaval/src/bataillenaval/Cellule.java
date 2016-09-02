de7d614fb55be84175c4f9e6472e1660897be3ef
Collot.Louis-Arthur <louis-arthur.collot@etudiant.univ-lille1.fr>
2016-09-02 14:11:09 +0200

    correction attaque

--- batailleNaval/src/bataillenaval/Cellule.java
+++ batailleNaval/src/bataillenaval/Cellule.java
@@ -2,43 +2,42 @@ package bataillenaval;
 
 public class Cellule {
 
-	private boolean toucher;
-	private Bateau bateau;
+    private boolean toucher = false;
+    private Bateau bateau;
 
-	public Cellule(boolean toucher) {
-		this.toucher = toucher;
-	}
+    public Cellule() {
 
-	public Cellule(){
-		this(false);
-	}
+    }
 
-	public boolean isToucher() {
-		return toucher;
-	}
+    public boolean isToucher() {
+        return toucher;
+    }
 
-	public boolean setToucher() {
-		this.toucher = true;
-		if(bateau!=null && !toucher){
-			bateau.bateauTouche();
-                        return true;
-		}
-                return false;
-	}
-
-	public boolean hasBateau(){
-		if(bateau==null)
-			return false;
-		return true;
-	}
-        
-	public void addBateau(Bateau b){
-		bateau=b;
-	}
-        
-        public boolean bateauCoule(){
-             if(bateau!=null)
-            return bateau.bateauCoule();
-             return false;
+    public boolean setToucher() {
+        if (bateau != null && !toucher) {
+            bateau.bateauTouche();
+            this.toucher = true;
+            return true;
         }
+        this.toucher = true;
+        return false;
+    }
+
+    public boolean hasBateau() {
+        if (bateau == null) {
+            return false;
+        }
+        return true;
+    }
+
+    public void addBateau(Bateau b) {
+        bateau = b;
+    }
+
+    public boolean bateauCoule() {
+        if (bateau != null) {
+            return bateau.bateauCoule();
+        }
+        return false;
+    }
 }
