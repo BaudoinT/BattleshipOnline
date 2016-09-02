98a7d62b7e693d3951ad734c66c3540a2b00eaf7
Collot.Louis-Arthur <louis-arthur.collot@etudiant.univ-lille1.fr>
2016-09-02 08:57:32 +0200

    correction

--- batailleNaval/src/bataillenaval/Affichage.java
+++ batailleNaval/src/bataillenaval/Affichage.java
@@ -12,53 +12,54 @@ import java.util.ArrayList;
  * @author collotl
  */
 public class Affichage {
-    private ArrayList<Cellule[][]> plateaux=new ArrayList<>();
+
+    private ArrayList<Cellule[][]> plateaux = new ArrayList<>();
 
     public Affichage(Cellule[][] plateauJ1, Cellule[][] plateauJ2) {
-       plateaux.add(plateauJ1);
-       plateaux.add(plateauJ2);
+        plateaux.add(plateauJ1);
+        plateaux.add(plateauJ2);
     }
 
-    public void affichagePlateau(int tour) {        
+    public void affichagePlateau(int tour) {
         System.out.println("\n\n");
-         
+
         this.affichageHautPlateau();
-        
+
         for (int i = 0; i < 10; i++) {
 
             System.out.print("| " + ((char) (65 + i)) + " ");
 
             System.out.print("|\u001B[30;40m");
             for (int x = 0; x < 9; x++) {
-                if (plateaux.get(1-tour)[i][x].bateauCoule()) {
+                if (plateaux.get(1 - tour)[i][x].bateauCoule()) {
                     System.out.print("\u001B[30;42m   \u001B[30;44m|");
-                }else if (plateaux.get(1-tour)[i][x].isToucher() && plateaux.get(1-tour)[i][x].hasBateau()) {
+                } else if (plateaux.get(1 - tour)[i][x].isToucher() && plateaux.get(1 - tour)[i][x].hasBateau()) {
                     System.out.print("\u001B[30;41m   \u001B[30;44m|");
-                } else if (plateaux.get(1-tour)[i][x].isToucher()) {
+                } else if (plateaux.get(1 - tour)[i][x].isToucher()) {
                     System.out.print("\u001B[30;47m   \u001B[30;44m|");
                 } else {
                     System.out.print("\u001B[30;44m   |");
                 }
 
             }
-            if (plateaux.get(1-tour)[i][9].bateauCoule()) {
-                    System.out.print("\u001B[30;42m   \u001B[30;44m|\u001B[30;47m");
-                }else if (plateaux.get(1-tour)[i][9].isToucher() && plateaux.get(1-tour)[i][9].hasBateau()) {
+            if (plateaux.get(1 - tour)[i][9].bateauCoule()) {
+                System.out.print("\u001B[30;42m   \u001B[30;44m|\u001B[30;47m");
+            } else if (plateaux.get(1 - tour)[i][9].isToucher() && plateaux.get(1 - tour)[i][9].hasBateau()) {
                 System.out.print("\u001B[30;41m   \u001B[30;44m|\u001B[30;47m");
-            }else if (plateaux.get(1-tour)[i][9].isToucher()) {
+            } else if (plateaux.get(1 - tour)[i][9].isToucher()) {
                 System.out.print("\u001B[30;47m   \u001B[30;44m|\u001B[30;47m");
             } else {
                 System.out.print("   |\u001B[30;47m");
             }
-            
+
             System.out.print("\n|___|");
-            
+
             for (int j = 0; j < 10; j++) {
-                if (plateaux.get(1-tour)[i][j].bateauCoule()) {
+                if (plateaux.get(1 - tour)[i][j].bateauCoule()) {
                     System.out.print("\u001B[30;42m___\u001B[30;44m|");
-                }else if (plateaux.get(1-tour)[i][j].isToucher() && plateaux.get(1-tour)[i][j].hasBateau()) {
+                } else if (plateaux.get(1 - tour)[i][j].isToucher() && plateaux.get(1 - tour)[i][j].hasBateau()) {
                     System.out.print("\u001B[30;41m___\u001B[30;44m|");
-                }else if (plateaux.get(1-tour)[i][j].isToucher()) {
+                } else if (plateaux.get(1 - tour)[i][j].isToucher()) {
                     System.out.print("\u001B[30;47m___\u001B[30;44m|");
                 } else {
                     System.out.print("\u001B[30;44m___|");
@@ -66,8 +67,7 @@ public class Affichage {
             }
             System.out.println("\u001B[30;47m");
         }
-        
-        
+
         this.affichageHautPlateau();
         for (int i = 0; i < 10; i++) {
 
@@ -75,9 +75,9 @@ public class Affichage {
 
             System.out.print("|\u001B[30;40m");
             for (int x = 0; x < 9; x++) {
-                 if (plateaux.get(1-tour)[i][x].bateauCoule()) {
+                if (plateaux.get(1 - tour)[i][x].bateauCoule()) {
                     System.out.print("\u001B[30;42m   \u001B[30;44m|");
-                }else if (plateaux.get(tour)[i][x].hasBateau()) {
+                } else if (plateaux.get(tour)[i][x].hasBateau()) {
                     System.out.print("\u001B[30;43m   \u001B[30;44m|");
                 } else if (plateaux.get(tour)[i][x].isToucher() && plateaux.get(tour)[i][x].hasBateau()) {
                     System.out.print("\u001B[30;41m   \u001B[30;44m|");
@@ -88,28 +88,28 @@ public class Affichage {
                 }
 
             }
-            if (plateaux.get(1-tour)[i][9].bateauCoule()) {
-                    System.out.print("\u001B[30;42m   \u001B[30;44m|\u001B[30;47m");
-                }else if (plateaux.get(tour)[i][9].hasBateau()) {
+            if (plateaux.get(1 - tour)[i][9].bateauCoule()) {
+                System.out.print("\u001B[30;42m   \u001B[30;44m|\u001B[30;47m");
+            } else if (plateaux.get(tour)[i][9].hasBateau()) {
                 System.out.print("\u001B[30;43m   \u001B[30;44m|\u001B[30;47m");
             } else if (plateaux.get(tour)[i][9].isToucher() && plateaux.get(tour)[i][9].hasBateau()) {
                 System.out.print("\u001B[30;41m   \u001B[30;44m|\u001B[30;47m");
-            }else if (plateaux.get(tour)[i][9].isToucher()) {
+            } else if (plateaux.get(tour)[i][9].isToucher()) {
                 System.out.print("\u001B[30;47m   \u001B[30;44m|\u001B[30;47m");
             } else {
                 System.out.print("   |\u001B[30;47m");
             }
-            
+
             System.out.print("\n|___|");
-            
+
             for (int j = 0; j < 10; j++) {
-                if (plateaux.get(1-tour)[i][j].bateauCoule()) {
+                if (plateaux.get(1 - tour)[i][j].bateauCoule()) {
                     System.out.print("\u001B[30;42m___\u001B[30;44m|");
-                }else  if (plateaux.get(tour)[i][j].hasBateau()) {
+                } else if (plateaux.get(tour)[i][j].hasBateau()) {
                     System.out.print("\u001B[30;43m___\u001B[30;44m|");
                 } else if (plateaux.get(tour)[i][j].isToucher() && plateaux.get(tour)[i][j].hasBateau()) {
                     System.out.print("\u001B[30;41m___\u001B[30;44m|");
-                }else if (plateaux.get(tour)[i][j].isToucher()) {
+                } else if (plateaux.get(tour)[i][j].isToucher()) {
                     System.out.print("\u001B[30;47m___\u001B[30;44m|");
                 } else {
                     System.out.print("\u001B[30;44m___|");
@@ -118,13 +118,12 @@ public class Affichage {
             System.out.println("\u001B[30;47m");
         }
     }
-    
-    
-    private void affichageHautPlateau(){
+
+    private void affichageHautPlateau() {
         for (int j = 0; j < 11; j++) {
             System.out.print(" ___");
         }
-        
+
         System.out.print("\n|");
         for (int i = 0; i < 11; i++) {
             if (i > 0 && i < 10) {
@@ -135,7 +134,7 @@ public class Affichage {
                 System.out.print("   |");
             }
         }
-        
+
         System.out.print("\n|");
         for (int i = 0; i < 11; i++) {
 
--- batailleNaval/src/bataillenaval/Jeu.java
+++ batailleNaval/src/bataillenaval/Jeu.java
@@ -2,134 +2,128 @@ package bataillenaval;
 
 public class Jeu {
 
-	private Plateau plat;
-	private Affichage aff;
-	int tourJoueur;
-	
-	public void lancerPartie(){
+    private Plateau plat;
+    private Affichage aff;
+    int tourJoueur;
 
-		//afhicherPlateau
-		plat=new Plateau();
-		aff=new Affichage(plat.getplateauJ1(),plat.getplateauJ2());
-		aff.affichagePlateau(0);
+    public void lancerPartie() {
 
-		tourJoueur=0;
-		choixBateauAPlacer(tourJoueur);
-		tourJoueur=(tourJoueur+1)%2;
-		choixBateauAPlacer(tourJoueur);
+        //afhicherPlateau
+        plat = new Plateau();
+        aff = new Affichage(plat.getplateauJ1(), plat.getplateauJ2());
+        aff.affichagePlateau(0);
 
+        tourJoueur = 0;
+        choixBateauAPlacer(tourJoueur);
+        tourJoueur = (tourJoueur + 1) % 2;
+        choixBateauAPlacer(tourJoueur);
 
-		//J1 joue (annonce touche coule)
-		new ToucheCoule(plat, aff).Attaque(tourJoueur);
-                
+        //J1 joue (annonce touche coule)
+        ToucheCoule touche = new ToucheCoule(plat, aff);
+        touche.Attaque(tourJoueur);
+
 		//test partie fini et annonce gagnant
-		//J2 joue ---------------------
-		//test partie fini et annonce gagnant
-
+        //J2 joue ---------------------
+        //test partie fini et annonce gagnant
 		//retour menu
+    }
 
-	}
+    public void choixBateauAPlacer(int tour) {
+        System.out.println("Au tour du joueur " + tourJoueur + 1 + " de placer ses bateaux !");
 
+        System.out.println("Vous avez 5 bateaux à placer: ");
+        System.out.println("1: Torpilleur (2 cases)");
+        System.out.println("2: Contre-torpilleur (3 cases)");
+        System.out.println("3: Sous-marin (3 cases)");
+        System.out.println("4: Croiseur (4 cases)");
+        System.out.println("5: Porte-avion (5 cases)");
 
-	public void choixBateauAPlacer(int tour){
-		System.out.println("Au tour du joueur "+tourJoueur+1+" de placer ses bateaux !");
+        int longueurBat[] = new int[]{2, 3, 3, 4, 5};
+        String nomBat[] = new String[]{"Torpilleur", "Contre-torpilleur", "Sous-marin", "Croiseur", "Porte-avion"};
 
-		System.out.println("Vous avez 5 bateaux à placer: ");
-		System.out.println("1: Torpilleur (2 cases)");
-		System.out.println("2: Contre-torpilleur (3 cases)");
-		System.out.println("3: Sous-marin (3 cases)");
-		System.out.println("4: Croiseur (4 cases)");
-		System.out.println("5: Porte-avion (5 cases)");
+        for (int i = 0; i < longueurBat.length; i++) {
+            System.out.println("Où voulez-vous placer le " + nomBat[i] + " (" + longueurBat[i] + " cases) ?");
+            emplacementBateau(longueurBat[i]);
+            aff.affichagePlateau(tour);
+        }
+    }
 
-		int longueurBat[]=new int[]{2,3,3,4,5};
-		String nomBat[]=new String[]{"Torpilleur","Contre-torpilleur","Sous-marin","Croiseur","Porte-avion"};
+    public void emplacementBateau(int longueur) {
+        Bateau bateau = new Bateau(longueur);
+        Saisir s;
+        String sens = "";
+        do {
+            System.out.println("Sur quelle case voulez-vous positionner votre bateau ?");
+            s = new Saisir();
+            s.choixCase();
+            System.out.println("Dans sens voulez-vous mettre votre bateau ?(1: Horizontal ou 2: Vertical)");
+            int choixSens = s.choixListe(2);
+            if (choixSens == 1) {
+                sens = "horizontal";
+            } else if (choixSens == 2) {
+                sens = "vertical";
+            }
 
-		for(int i=0; i<longueurBat.length; i++){
-			System.out.println("Où voulez-vous placer le "+nomBat[i]+" ("+longueurBat[i]+" cases) ?");
-			emplacementBateau(longueurBat[i]);
-			aff.affichagePlateau(tour);
-		}
-	}
+        } while (!verifEtPlacement(longueur, s.getLigne(), s.getCol(), sens, bateau));
 
-	public void emplacementBateau(int longueur){
-		Bateau bateau=new Bateau(longueur);
-		Saisir s;
-		String sens="";
-		do{
-			System.out.println("Sur quelle case voulez-vous positionner votre bateau ?");
-			s = new Saisir();
-			s.choixCase();
-			System.out.println("Dans sens voulez-vous mettre votre bateau ?(1: Horizontal ou 2: Vertical)");
-			int choixSens=s.choixListe(2);
-			if(choixSens==1){
-				sens="horizontal";
-			}else if(choixSens==2){
-				sens="vertical";
-			}
+    }
 
-		}while(!verifEtPlacement(longueur, s.getLigne(), s.getCol(), sens, bateau));
+    public boolean verifEtPlacement(int longueur, int posX, int posY, String sens, Bateau bateau) {
+        if (sens.equals("horizontal")) {
+            if (posY + longueur <= 10) {
 
+                if (tourJoueur == 0) {
+                    for (int i = posY; i < posY + longueur; i++) {
+                        if (plat.getplateauJ1()[posX][i].hasBateau()) {
+                            System.out.println("Le bateau ne peut pas être placé ici, recommencez !");
+                            return false;
+                        }
+                    }
+                    for (int i = posY; i < posY + longueur; i++) {
+                        plat.getplateauJ1()[posX][i].addBateau(bateau);
+                    }
+                } else if (tourJoueur == 1) {
+                    for (int i = posY; i < posY + longueur; i++) {
+                        if (plat.getplateauJ2()[posX][i].hasBateau()) {
+                            System.out.println("Le bateau ne peut pas être placé ici, recommencez !");
+                            return false;
+                        }
+                    }
+                    for (int i = posY; i < posY + longueur; i++) {
+                        plat.getplateauJ2()[posX][i].addBateau(bateau);
+                    }
+                }
+                return true;
+            }
+        } else if (sens.equals("vertical")) {
+            if (posX + longueur <= 10) {
 
+                if (tourJoueur == 0) {
+                    for (int i = posX; i < posX + longueur; i++) {
+                        if (plat.getplateauJ1()[i][posY].hasBateau()) {
+                            return false;
+                        }
+                    }
+                    for (int i = posX; i < posX + longueur; i++) {
+                        plat.getplateauJ1()[i][posY].addBateau(bateau);
+                    }
+                } else if (tourJoueur == 1) {
+                    for (int i = posX; i < posX + longueur; i++) {
+                        if (plat.getplateauJ2()[i][posY].hasBateau()) {
+                            return false;
+                        }
+                    }
+                    for (int i = posX; i < posX + longueur; i++) {
+                        plat.getplateauJ2()[i][posY].addBateau(bateau);
+                    }
+                }
+                return true;
+            }
+        } else {
+            System.out.println("Cette place n'est pas valide !");
+            return false;
+        }
+        return false;
 
-	}
-
-	public boolean verifEtPlacement(int longueur, int posX, int posY, String sens, Bateau bateau){
-		if(sens.equals("horizontal")){
-			if(posY+longueur <= 10){
-
-
-				if(tourJoueur==0){
-					for(int i=posY; i<posY+longueur; i++){
-						if(plat.getplateauJ1()[posX][i].hasBateau()){
-							System.out.println("Le bateau ne peut pas être placé ici, recommencez !");
-							return false;
-						}
-					}
-					for(int i=posY; i<posY+longueur; i++){
-						plat.getplateauJ1()[posX][i].addBateau(bateau);
-					}
-				}else if(tourJoueur==1){
-					for(int i=posY; i<posY+longueur; i++){
-						if(plat.getplateauJ2()[posX][i].hasBateau()){
-							System.out.println("Le bateau ne peut pas être placé ici, recommencez !");
-							return false;
-						}
-					}
-					for(int i=posY; i<posY+longueur; i++){
-						plat.getplateauJ2()[posX][i].addBateau(bateau);
-					}
-				}
-				return true;
-			}
-		}else if (sens.equals("vertical")){
-			if(posX+longueur <= 10){	
-
-				if(tourJoueur==0){
-					for(int i=posX; i<posX+longueur; i++){
-						if(plat.getplateauJ1()[i][posY].hasBateau()){
-							return false;
-						}
-					}
-					for(int i=posX; i<posX+longueur; i++){
-						plat.getplateauJ1()[i][posY].addBateau(bateau);
-					}
-				}else if(tourJoueur==1){
-					for(int i=posX; i<posX+longueur; i++){
-						if(plat.getplateauJ2()[i][posY].hasBateau()){
-							return false;
-						}
-					}
-					for(int i=posX; i<posX+longueur; i++){
-						plat.getplateauJ2()[i][posY].addBateau(bateau);
-					}
-				}
-				return true;
-			}
-		}else{
-			System.out.println("Cette place n'est pas valide !");
-			return false;
-		}
-		return false;
-
-	}
+    }
 }
